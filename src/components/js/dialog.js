c5bc223bbf500084663b6c8ec9af4867d6f0688c
陆唯超 <l@dpjia.com>
2020-07-15 15:14:40 +0800

    init

---
+++ src/components/js/dialog.js
@@ -0,0 +1,53 @@
+import Util from "./util.js"
+
+function onbuttonclick(idStr) {
+    switch (idStr) {
+        case "getDocName": {
+            let doc = wps.EtApplication().ActiveWorkbook
+            if (!doc) {
+                return "当前没有打开任何文档"
+            }
+            return doc.Name;
+        }
+        case "createTaskPane": {
+            let tsId = wps.PluginStorage.getItem("taskpane_id")
+            if (!tsId) {
+                let tskpane = wps.CreateTaskPane(Util.GetUrlPath() + "taskpane")
+                let id = tskpane.ID
+                wps.PluginStorage.setItem("taskpane_id", id)
+                tskpane.Visible = true
+            } else {
+                let tskpane = wps.GetTaskPane(tsId)
+                tskpane.Visible = true
+            }
+            break
+        }
+        case "newDoc": {
+            wps.EtApplication().Workbooks.Add()
+            break
+        }
+        case "addString": {
+            let curSheet = wps.EtApplication().ActiveSheet;
+            if (curSheet) {
+                curSheet.Cells.Item(1, 1).Formula = "Hello, wps加载项!" + curSheet.Cells.Item(1, 1).Formula
+            }
+            break;
+        }
+        case "closeDoc": {
+            if (wps.EtApplication().Workbooks.Count < 2) {
+                alert("当前只有一个文档，别关了。")
+                break
+            }
+
+            let doc = wps.EtApplication().ActiveWorkbook
+            if (doc)
+                doc.Close()
+            break
+        }
+    }
+
+}
+
+export default {
+    onbuttonclick
+}
